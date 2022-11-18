1b3546077cf193d59780c59c0013c35abec05677
Mathews <mathewsrodrigues9@gmail.com>
2022-11-18 16:32:40 -0300

    requisito 3 concluido

--- src/Energy.ts
+++ src/Energy.ts
@@ -1,9 +1,6 @@
-export type EnergyType = {
-  mana: string;
-  stamina: string;
-};
+export type EnergyType = 'mana' | 'stamina';
 
 export default interface Energy {
-  type: EnergyType;
+  type_: EnergyType;
   amount: number;
 }
