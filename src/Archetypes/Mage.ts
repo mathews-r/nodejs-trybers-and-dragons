56cde40433d0c646696a4b477d6a674e3823628e
Mathews <mathewsrodrigues9@gmail.com>
2022-11-18 17:18:47 -0300

    requisito 5 concluido

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetypes from './Archetypes';
+
+class Mage extends Archetypes {
+  private static _count = 0;
+  private _energyType: EnergyType;
+
+  constructor(name: string, energyType: EnergyType = 'mana') {
+    super(name);
+    this._energyType = energyType;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    this._count += 1;
+    return this._count;
+  }
+}
+
+export default Mage;
---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetypes from './Archetypes';
+
+class Warrior extends Archetypes {
+  private static _count = 0;
+  private _energyType: EnergyType;
+
+  constructor(name: string, energyType: EnergyType = 'stamina') {
+    super(name);
+    this._energyType = energyType;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+
+  static createdArchetypeInstances(): number {
+    this._count += 1;
+    return this._count;
+  }
+}
+
+export default Warrior;
