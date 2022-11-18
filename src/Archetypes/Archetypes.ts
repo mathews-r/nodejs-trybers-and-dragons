f4f395e66853729ba2178d1da638566b6ca9b77e
Mathews <mathewsrodrigues9@gmail.com>
2022-11-18 16:44:53 -0300

    requisito 4 concluido

--- src/Archetypes/Archetypes.ts
+++ src/Archetypes/Archetypes.ts
@@ -1,3 +1,5 @@
+import { EnergyType } from '../Energy';
+
 export default abstract class Archetypes {
   private _name: string;
   private _special: number;
@@ -12,4 +14,18 @@ export default abstract class Archetypes {
   get name(): string {
     return this._name;
   }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
+  }
+
+  static createdArchetypeInstances(): number {
+    throw new Error('Not implemented');
+  }
+
+  abstract get energyType(): EnergyType;
 }
