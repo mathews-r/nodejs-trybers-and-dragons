fb3360a325c9157f4bbf712fb1474bc39f5c434d
Mathews <mathewsrodrigues9@gmail.com>
2022-11-19 12:04:51 -0300

    requisito 12 concluido

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -4,8 +4,8 @@ import Battle from './Battle';
 class PVP extends Battle {
   private _player2: Fighter;
 
-  constructor(player1: Fighter, player2: Fighter) {
-    super(player1);
+  constructor(player: Fighter, player2: Fighter) {
+    super(player);
     this._player2 = player2;
   }
 
@@ -13,7 +13,7 @@ class PVP extends Battle {
     while (this.player.lifePoints !== -1 || this._player2.lifePoints !== -1) {
       this.player.attack(this._player2);
       this._player2.attack(this.player);
-      if (this._player2.lifePoints === -1 || this.player.lifePoints === -1) {
+      if (this.player.lifePoints === -1 || this._player2.lifePoints === -1) {
         break;
       } 
     }
